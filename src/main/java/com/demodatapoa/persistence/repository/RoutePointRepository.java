cf78b55ac02131f5686a6c2f4f2d2c581337a996
Victor Krause <victor5887@gmail.com>
2020-08-10 05:27:26 -0300

    Route Point (criação intinerário) - remoção parcial

--- src/main/java/com/demodatapoa/persistence/repository/RoutePointRepository.java
+++ src/main/java/com/demodatapoa/persistence/repository/RoutePointRepository.java
@@ -1,12 +1,12 @@
-package com.demodatapoa.persistence.repository;
-
-import com.demodatapoa.persistence.model.Route;
-import com.demodatapoa.persistence.model.RoutePoint;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface RoutePointRepository extends CrudRepository<RoutePoint, Long> {
-
-    List<RoutePoint> findByRoute(Route route);
-}
+//package com.demodatapoa.persistence.repository;
+//
+//import com.demodatapoa.persistence.model.Route;
+//import com.demodatapoa.persistence.model.RoutePoint;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.List;
+//
+//public interface RoutePointRepository extends CrudRepository<RoutePoint, Long> {
+//
+//    List<RoutePoint> findByRoute(Route route);
+//}
